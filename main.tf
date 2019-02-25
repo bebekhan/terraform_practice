6649224c00780dd16531c8c9e724c9ad389070c0
benish sarinelli <benishsarinelli@benishs-MacBook-Pro.local>
2019-02-25 11:54:42 -0800

    Updated security group and port configuration

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+terraform {
+	required_version = ">= 0.8, < 0.9"
+}
+
 provider "aws" {
 	region = "us-east-1"
 }
@@ -5,6 +9,13 @@ provider "aws" {
 resource "aws_instance" "example" {
 	ami           = "ami-40d28157"
 	instance_type = "t2.micro" 
+	vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+	user_data = <<-EOF
+				#!/bin/bash
+				echo "HELLO WORLD!!!" > index.html
+				nohup busybox httpd -f -p 8080 &
+				EOF
 
 	tags {
 	  Name = "terraform-example"
@@ -12,3 +23,18 @@ resource "aws_instance" "example" {
 	}
 }
 
+resource  "aws_security_group" "instance" {
+	name = "terraform-example-instance"
+
+	ingress {
+	  from_port  = 8080
+	  to_port    = 8080
+	  protocol   = "tcp"
+	  cidr_blocks = ["0.0.0.0/0"]
+	}
+}
+
+
+output "public_ip" {
+	value = "${aws_instance.example.public_ip}"
+}
